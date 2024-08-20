dcd4d6a0675803ec1f1129ac51e4c90fe2f3e3dc
minhazrwe <minhaz.rahman@rwe.com>
2024-08-20 09:03:36 +0200

    new4

--- dbo/StoredProcedures/TEST.sql
+++ dbo/StoredProcedures/TEST.sql
@@ -8,8 +8,8 @@ INSERT INTO [FinRecon].[dbo].[TEST1] -- HIADKJAJ
            ([Test]) -- new changed 3
      VALUES
            (1)
+-- A new change by Minhaz
+--NEW CHANGED
 
 GO
 
--- A new change by Minhaz
---NEW CHANGED
